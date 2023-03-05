456538aeceea3f8c767cc0facc923405dd4d4973
aline-m-dias <martinsaline558@gmail.com>
2023-03-05 19:28:58 -0300

    UpdateCidadeController.js funcionando

---
+++ Atividades/atividade-pratica-01/server/src/controller/cidades/UpdateCidadeController.js
@@ -0,0 +1,28 @@
+import { prisma } from '../../database/client.js';
+
+export class UpdateCidadeController{
+    
+    async handle(request, response) {
+
+        const {id, nome, estado_id} = request.body;
+
+        const cidade = await prisma.cidade.update({
+            where:{
+                id: parseInt (id)
+            }, 
+
+            data: {
+                nome,
+                estado: {
+                    connect: {
+                        id: estado_id 
+                    }
+                },
+                updated_at: new Date()
+            }
+          
+        });
+        response.json(cidade);
+    }
+ 
+}
--- Atividades/atividade-pratica-01/server/src/routes/cidade.js
+++ Atividades/atividade-pratica-01/server/src/routes/cidade.js
@@ -2,16 +2,16 @@ import { Router } from "express";
 
 import { GetAllCidadeController } from "../controller/cidades/GetAllCidadeController.js";
 import { CreateCidadeController } from "../controller/cidades/CreateCidadeController.js";
+import { UpdateCidadeController } from "../controller/cidades/UpdateCidadeController.js";
 
 const cidadeRouter = Router();
 
 const getAllCidadeController = new GetAllCidadeController();
 const createCidadeController = new CreateCidadeController();
+const updateCidadeController = new UpdateCidadeController();
 
-
-// Get All 
 cidadeRouter.get('/cidades', getAllCidadeController.handle);
-// Create - Post
 cidadeRouter.post('/cidades',createCidadeController.handle);
+cidadeRouter.put('/cidades',updateCidadeController.handle);
 
 export {cidadeRouter}
